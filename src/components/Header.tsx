65d6ed0e0b2450782f585a94ec6ca15197b95054
Jobert Mampusti <mampustijobert@gmail.com>
2025-06-10 01:38:30 +0800

    feat: adjusted text size in navbar and added hover effects

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,28 @@
 function Header() {
   return (
-    <header className="text-white font-bold flex justify-between items-center py-6 mx-16">
+    <header className="text-white flex justify-between items-center py-6 mx-16">
       {/* left section */}
-      <p className="text-2xl">TreBojx</p>
+      <p className="text-2xl font-bold">TreBojx</p>
       {/* right section */}
       <div className="flex items-center w-2xl justify-between">
         <nav className="text-xl flex justify-around w-lg">
-          <a href="">Home</a>
-          <a href="">About</a>
-          <a href="">Projects</a>
-          <a href="">Skills</a>
-          <a href="">Contact</a>
+          <a href="" className="hover:text-blue-400">
+            Home
+          </a>
+          <a href="" className="hover:text-blue-400">
+            About
+          </a>
+          <a href="" className="hover:text-blue-400">
+            Projects
+          </a>
+          <a href="" className="hover:text-blue-400">
+            Skills
+          </a>
+          <a href="" className="hover:text-blue-400">
+            Contact
+          </a>
         </nav>
-        <button className="bg-blue-600 text-xl px-6 py-2 rounded-full">
+        <button className="bg-blue-600 font-bold text-xl px-6 py-2 rounded-full cursor-pointer hover:bg-blue-800">
           Resume
         </button>
       </div>
